9b00fd7e041965800600d38614aae1ba60c632a5
Angel Leon <elonlegan@gmail.com>
2020-08-31 21:23:29 -0500

    Done

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,18 @@
 const FOOTER = document.getElementById("footer");
 const SHARE = document.getElementById("share");
 const ICON = document.getElementById("icon-share");
+const AVATAR = document.getElementById("avatar");
 
 const changeClassName = () => {
   if (ICON.classList.contains("active")) {
     FOOTER.classList.remove("active");
     SHARE.classList.remove("active");
     ICON.classList.remove("active");
+    AVATAR.classList.remove("active");
   } else {
     FOOTER.classList.add("active");
     SHARE.classList.add("active");
     ICON.classList.add("active");
+    AVATAR.classList.add("active");
   }
 };
